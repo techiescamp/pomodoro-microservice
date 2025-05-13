207fdcb42f52c96651f5772aeac25f55b9e6420c
SravyaCrunchOps <sravya@crunchops.com>
2025-05-13 10:20:21 +0530

    :bug added proxy server at fe

--- fe-pomodoro-app/src/config.js
+++ fe-pomodoro-app/src/config.js
@@ -1,8 +1,6 @@
 
 const config = {
     apiUrl: process.env.REACT_APP_API_URL,
-    reportsUrl: process.env.REACT_APP_REPORTS_API_URL,
-    metrics_url: process.env.REACT_APP_METRICS_URL,
     jaeger_trace_url: process.env.REACT_APP_JAEGER_TRACE_URL,
     uiUrl: process.env.REACT_APP_UI_URL
 }
